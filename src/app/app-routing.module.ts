e750e3f0e7764c1ca15eaa3817909ea91f02a669
Breno do Val <brenoribeiro@procobservicos.com.br>
2021-03-23 13:58:59 -0300

    feat(scaffold): add project scaffold

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { BookmarksPage } from './pages/bookmarks/bookmarks.page';
+import { HomePage } from './pages/home/home.page';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+    {
+      path: '', component: HomePage
+    },
+    {
+      path: 'bookmarks', component: BookmarksPage
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: 'ae57cd3e181166f41712beec1225a284'
+};
